5b7a1240ed8994d9eaab762ce2a272607133bae6
Dalbir Rana <dalbirsrana@gmail.com>
2020-12-11 20:23:09 -0800

    git init

---
+++ client/src/components/personsBoats.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { GET_PEOPLE_BOATS } from '../../queries'
+import { useQuery } from '@apollo/client'
+
+const PersonWithBoats = ({ display, id }) => {
+
+    const styles = ({
+        container: {
+            display: display,
+            position: 'relative',
+            top: '20px',
+        },
+        list: {
+            listStyleType: 'none',
+            display: 'grid',
+            gridGap: '10px',
+            padding: '10px'
+        },
+        child: {
+            border: 'solid 1px',
+            padding:'10px'
+        },
+        grey: {
+            color: 'grey'
+        }
+    })
+
+    const { loading, error, data } = useQuery(GET_PEOPLE_BOATS, { variables: { personId: id } })
+    if (loading) return '..'
+    if (error) return `Errror! ${error.message}`
+    return (
+        <div style={styles.container}>
+            {console.log('data=>', data.personWithboats)}
+            <h3>My Boats</h3>
+            <ul style={styles.list}>
+                {
+                    data.personWithboats.map(item => {
+                        return <li style={styles.child}> <span style={styles.grey}>Model:</span> {item.model}, <span style={styles.grey}>Year:</span> {item.year}, <span style={styles.grey}>Price:</span> ${item.price}, <span style={styles.grey}>Make:</span>{item.make}</li>
+                    })
+                }
+            </ul>
+        </div>
+    )
+}
+
+export default PersonWithBoats;
---
+++ client/src/queries/index.js
@@ -0,0 +1,94 @@
+import { gql } from '@apollo/client'
+
+export const GET_PEOPLE = gql`
+  {
+    people {
+      id
+      firstName
+      lastName
+    }
+  }
+`
+
+export const GET_PEOPLE_BOATS = gql`
+  {
+    personWithboats {
+      id
+      year
+      make
+      model
+      price
+      personId
+    }
+  }
+`
+
+export const ADD_PERSON = gql`
+  mutation AddPerson($id: String!, $firstName: String!, $lastName: String!) {
+    addPerson(id: $id, firstName: $firstName, lastName: $lastName) {
+      id
+      firstName
+      lastName
+    }
+  }
+`
+
+export const UPDATE_PERSON = gql`
+  mutation UpdatePerson($id: String!, $firstName: String!, $lastName: String!) {
+    updatePerson(id: $id, firstName: $firstName, lastName: $lastName) {
+      id
+      firstName
+      lastName
+    }
+  }
+`
+
+export const REMOVE_PERSON = gql`
+  mutation RemovePerson($id: String!) {
+    removePerson(id: $id) {
+      id
+      firstName
+      lastName
+    }
+  }
+`
+
+
+export const ADD_BOATS = gql`
+  mutation AddBoats($id: String!, $year: String!, $make: String!, $model: String!, $price: String!, , $personId: String!) {
+    addBoats(id: $id, year: $year, make: $make, model: $model, price: $price, personId: $personId) {
+      id
+      year
+      make
+      model
+      price
+      personId
+    }
+  }
+`
+
+export const REMOVE_BOAT = gql`
+  mutation RemoveBoat($id: String!) {
+    removeboat(id: $id) {
+      id
+      year
+      make
+      model
+      price
+      personId
+    }
+  }
+`
+
+export const REMOVE_ALL_BOATS_WITH_PERSON = gql`
+  mutation RemoveAllBoatsWithPerson($personId: String!) {
+    removeAllBoatsWithPerson(personId: $personId) {
+      id
+      year
+      make
+      model
+      price
+      personId
+    }
+  }
+`
